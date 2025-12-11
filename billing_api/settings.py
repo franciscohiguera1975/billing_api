2806006025056ecaa0885f2f003c8b9288005a7c
Francisco Higuera <franciscohiguera@gmail.com>
2025-12-11 14:43:52 -0500

    statics

--- billing_api/settings.py
+++ billing_api/settings.py
@@ -133,7 +133,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Solo si tienes archivos estáticos adicionales
+]
+
+# Media files (si necesitas subir archivos)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
